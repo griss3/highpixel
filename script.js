bef825681ddd87bb49d63e94d630e982cd78ecd8
Sergio <grisse.dls@gmail.com>
2023-11-30 09:27:15 -0400

    primera actualizacion codigo separado

---
+++ script.js
@@ -0,0 +1,5 @@
+function cambiarContenido(categoria) {
+    // Aquí puedes realizar acciones específicas según la categoría
+    // Por ahora, simplemente cambiaremos el texto de ejemplo
+    document.getElementById('contenido').innerHTML = `<p>Contenido de ${categoria}</p>`;
+}
